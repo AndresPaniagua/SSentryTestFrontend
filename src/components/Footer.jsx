430f2df7a3a3379f295eaeac7a852fc781fb35d7
AndresPaniagua <andresfelipep.l14@gmail.com>
2022-02-09 08:11:59 -0500

    Fix features

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Navbar, Container } from "react-bootstrap";
+import LinkedinLogo from '../assets/images/Linkedin.png';
 
 export default function Footer() {
   return (
     <Navbar expand="lg" variant="light" bg="light" fixed="bottom">
       <Container className="justify-content-center">
-        <Navbar.Brand href="#">
-          Andrés Felipe Paniagua Lema - .Net Developer:
-          <a className="text-black" href="https://github.com/AndresPaniagua">
-            Own Repository
+        <Navbar.Brand>
+          <a href="https://www.linkedin.com/in/andres-felipe-paniagua-lema-89236a163/" 
+            target="_blank"
+            style={{textDecoration: 'none'}}>
+            <img src={LinkedinLogo} alt="Linkedin" width={25}/> 
+            <span> </span>
           </a>
+
+          Andrés Felipe Paniagua Lema - .Net Developer:
+          
+          <a className="text-black" href="https://github.com/AndresPaniagua" target="_blank">
+          <span> Own Repository </span>
+          </a>
+          
           © 2022
         </Navbar.Brand>
       </Container>
